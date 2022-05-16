22ba0e73d34e36d7e5f8fa85fdd3216a8cf5ed31
jtompkins415 <john.tompkins415@gmail.com>
2022-05-16 15:26:34 -0700

    initialize exercise files

---
+++ app.js
@@ -0,0 +1,18 @@
+// var PI = 3.14;
+// PI = 42;
+
+const PI = 3.14;
+
+
+//What is the difference between var and let?
+// var allows redeclaration and reassigning of the same variable. let allows a variable to be reassigned, but not redeclared
+
+//What is the difference between var and const?
+// const prevents a variable from being redeclared or reassigned. Arrays and objects within a variable declared with const can be manipulated but the reference or name of variable cannot be changed
+
+//What is the difference between let and const?
+// let allows reassining of variables, while const is incredibly strict and doesnt allow any redeclaration or reassigning. 
+
+//What is Hoisting?
+// In JS variables are declared w/ var are "hoisted" to the top of the script. The variables are declared, but are not given a value. Hoisting does not apply to variables declared w/ let and const. 
+
